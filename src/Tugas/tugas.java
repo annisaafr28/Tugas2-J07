1e29b0bc21265f125591ef5e24addf46e2e7d49b
DELL <DELL@DELL-PC>
2018-09-13 13:19:52 +0700

    tambah tampil hasil

--- src/Tugas/tugas.java
+++ src/Tugas/tugas.java
@@ -16,6 +16,7 @@ public class tugas {
         tampilJudul(identitas);
         int n = tampilInput();
         BigInteger hasil = fibo(n);
+        tampilHasil(n, hasil);
     }
     
         private static int tampilInput() {
@@ -38,5 +39,10 @@ public class tugas {
                 hasil[i] = hasil[i-1].add(hasil[i-2]);
             }
             return hasil[n-1];
+    }
+        private static void tampilHasil(int n, BigInteger hasil) {
+            System.out.println("Bilangan Fibonacci ke-" + n + " : " + hasil);
+            
         }
+        
 }
