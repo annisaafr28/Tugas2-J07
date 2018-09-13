e44d99913dbae6112298099880448844baee0cca
DELL <DELL@DELL-PC>
2018-09-13 13:04:53 +0700

    initial commit

---
+++ src/Tugas/tugas.java
@@ -0,0 +1,18 @@
+
+package Tugas;
+import java.util.Scanner;
+public class tugas {
+    private static void tampilJudul(String identitas) {
+        System.out.println("Identitas : " + identitas);
+        
+        System.out.println("\nhitung Fibonacci");
+        System.out.println("1, 1, 2, 3, 5, 13, 21, ... dst.\n");
+    }
+    
+    public static void main(String[] args) {
+        String identitas = "Annisa Fairuz Rizqi / X RPL 2 / 07";
+        
+        tampilJudul(identitas);
+        
+    }
+}
