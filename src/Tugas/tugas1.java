f7f62a52d46dfab51b61519c2afca80f6f425d54
DELL <DELL@DELL-PC>
2018-09-14 20:26:14 +0700

    initial commit

---
+++ src/Tugas/tugas1.java
@@ -0,0 +1,9 @@
+
+package Tugas;
+import java.util.Scanner;
+public class tugas1 {
+   public static void main(String[] args) {
+       int[] nominal = {5000, 2000, 1000, 500, 100};
+      
+   }
+}
