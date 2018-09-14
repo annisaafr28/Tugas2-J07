36b91f15e38a65f99a7db596ed6407000bdad502
DELL <DELL@DELL-PC>
2018-09-14 21:21:55 +0700

    tambah array dan deklarasi variabel

--- src/Tugas/tugas2.java
+++ src/Tugas/tugas2.java
@@ -6,6 +6,10 @@ public class tugas2 {
         String identitas = "Annisa Fairuz Rizqi / X RPL 2 / 07";
         System.out.println("Identitas : " + identitas);
         
+        Scanner sistem = new Scanner(System.in);
+        int arr[] = {7,5,4,3,2};
+        int arr1[] = {10000, 7000, 5000, 3000, 2000};
+        int bayaran = 0;
         
         
     }
