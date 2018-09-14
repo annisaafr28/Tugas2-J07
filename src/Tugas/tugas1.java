fadc536263553a63c95f6ccfd40811c196915e8f
DELL <DELL@DELL-PC>
2018-09-14 20:27:21 +0700

    tambah input jumlah uang

--- src/Tugas/tugas1.java
+++ src/Tugas/tugas1.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 public class tugas1 {
    public static void main(String[] args) {
        int[] nominal = {5000, 2000, 1000, 500, 100};
-      
+       
+       Scanner scan = new Scanner(System.in);
+       System.out.print("Masukkan jumlah uang : ");
+       int uang = scan.nextInt();
+       
    }
 }
