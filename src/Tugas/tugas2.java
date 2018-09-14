05ad292cc2a3ceb7ec15c012ca4551124c8e6395
DELL <DELL@DELL-PC>
2018-09-14 21:21:00 +0700

    initial commit

---
+++ src/Tugas/tugas2.java
@@ -0,0 +1,12 @@
+
+package Tugas;
+import java.util.Scanner;
+public class tugas2 {
+    public static void main(String[] args) {
+        String identitas = "Annisa Fairuz Rizqi / X RPL 2 / 07";
+        System.out.println("Identitas : " + identitas);
+        
+        
+        
+    }
+}
